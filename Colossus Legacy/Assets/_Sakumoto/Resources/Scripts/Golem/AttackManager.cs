7da6d7c023ecda6893636165efe25b6f47592a10
1204014 <kd1331533@st.kobedenshi.ac.jp>
2024-05-14 11:37:12 +0900

    AttackManager ちょい更新

--- Colossus Legacy/Assets/_Sakumoto/Resources/Scripts/Golem/AttackManager.cs
+++ Colossus Legacy/Assets/_Sakumoto/Resources/Scripts/Golem/AttackManager.cs
@@ -16,40 +16,43 @@ public class AttackManager : MonoBehaviour
 {
     public class AttackData
     {
-        public int id = 0;                  // 攻撃ID
-        public bool isAttack = false;       // 攻撃している状態か
-        public bool enable = false;         // 攻撃判定
-        public float coolTime = 0.0f;       // クールタイム
+        public int m_id { get; set; }             // 攻撃ID
+        public bool m_enable { get; set; }        // 攻撃モーションをしているか
+        public bool m_isAttack { get; set; }      // 攻撃判定出現状態か
+        public float m_coolTime { get; set; }     // クールタイム
+        public float m_startTime {  get; set; }   // 攻撃判定開始フレーム
+        public float m_endTime { get; set; }      // 攻撃判定終了フレーム
     }
+    public AttackData m_instance { get; set; }
 
     private bool m_canAttack = false;     // 攻撃可能状態かの判定用
     private float m_coolDown = 0.0f;      // 次の攻撃までのクールダウン時間
     private List<AttackData> m_attackLists = new List<AttackData>();
     
     // 攻撃追加処理   ：   攻撃をリストに追加する
-    void AddAttack(AttackData _attack)
+    public void AddAttack(AttackData _attack)
     {
         m_attackLists.Add(_attack);
     }
 
 
     // 攻撃発生処理   ：   引数の ID の攻撃を発生させてクールダウン時間を設定
-    void Action(int _attackId = 0)
+    public void Action(int _attackId = 0)
     {
         int id = SearchAttackId(_attackId);
 
-        m_coolDown = m_attackLists[id].coolTime;
+        m_coolDown = m_attackLists[id].m_coolTime;
     }
 
 
-    // リスト内の対象IDを探して、要素数を返す
+    // リスト内の対象IDを探して、インデックス番号を返す
     int SearchAttackId(int _attackId)
     {
         int result = 0;
 
         for (int i = 0; i < m_attackLists.Count; i++)
         {
-            if (m_attackLists[i].id == _attackId)
+            if (m_attackLists[i].m_id == _attackId)
             {
                 result = i;
                 break;
@@ -57,5 +60,12 @@ public class AttackManager : MonoBehaviour
         }
 
         return result;
+    }
+
+
+    // インスタンスを初期化
+    private void Awake()
+    {
+        m_instance = new AttackData();   
     }
 }

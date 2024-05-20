cd526261a28ecf9cb9d5599e5d7c30c309f7f4c4
kikuondesu <kd1314178@st.kobedenshi.ac.jp>
2024-05-20 14:30:24 +0900

    西田:コミットし忘れていたスクリプトをアップ

--- Colossus Legacy/Assets/_Nishida/Scripts/AllowMovement.cs
+++ Colossus Legacy/Assets/_Nishida/Scripts/AllowMovement.cs
@@ -8,12 +8,14 @@ public class AllowMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // 親オブジェクト(主人公)のTransform取得
         parentTransform = transform.parent;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 常に主人公の上で同じ向きになるようにする処理
         transform.rotation = Quaternion.Euler(0, -parentTransform.rotation.y, 0);
     }
 }

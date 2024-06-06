c5b0861ac5bc6b2e417d3c3e7ca7c44fcdc67355
1204014 <kd1331533@st.kobedenshi.ac.jp>
2024-06-06 14:11:48 +0900

    弱点部位のスクリプト

---
+++ Colossus Legacy/Assets/_Sakumoto/Resources/Scripts/Golem/WeakPoint.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakPoint : MonoBehaviour
+{
+    public bool m_weakHit { get; set; }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("SwordAttack"))
+        {
+            Debug.Log("ƒS[ƒŒƒ€‚Ìã“_‚ªUŒ‚‚³‚ê‚½I");
+            m_weakHit = true;
+        }
+    }
+}

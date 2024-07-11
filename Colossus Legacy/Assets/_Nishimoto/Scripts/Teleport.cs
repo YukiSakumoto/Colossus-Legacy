7cc01d42a4cbb44b7e2054d8eebe2ca560f63dac
Aqch022 <seiryu822114@gmail.com>
2024-07-11 14:39:50 +0900

    テレポートギミック作成(未完成)

--- Colossus Legacy/Assets/_Nishimoto/Scripts/Teleport.cs
+++ Colossus Legacy/Assets/_Nishimoto/Scripts/Teleport.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-    [SerializeField] GameObject obj;
+    GameObject obj;
+    [SerializeField] GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +16,13 @@ public class Teleport : MonoBehaviour
     void Update()
     {
         
+    }
+    public void SetTagCollision(GameObject _obj)
+    {
+        obj = _obj;
+    }
+    public void TeleportNext()
+    {
+        Player.transform.position = obj.transform.position;
     }
 }
